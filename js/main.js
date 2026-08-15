909143b661a211518a49d1669d34c90ae12ac429
pedroyov <pedroyovera.21@gmail.com>
2026-08-15 13:14:20 -0500

    updated icon-app

--- js/main.js
+++ js/main.js
@@ -140,3 +140,23 @@ if (shareBtn) {
     }
   });
 }
+
+
+if ("serviceWorker" in navigator) {
+  window.addEventListener("load", () => {
+    navigator.serviceWorker
+      .register("./service-worker.js")
+      .then((registration) => {
+        console.log(
+          "Service Worker registrado:",
+          registration.scope
+        );
+      })
+      .catch((error) => {
+        console.error(
+          "Error al registrar el Service Worker:",
+          error
+        );
+      });
+  });
+}
